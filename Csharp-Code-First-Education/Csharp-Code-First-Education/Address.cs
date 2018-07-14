bdd79bb9765a18cc976c4aec45020d3688222621
NajibAJama <Najib.A.Jama@gmail.com>
2018-07-14 03:03:18 -0700

    Configuring Unconventional Complex Types

--- Csharp-Code-First-Education/Csharp-Code-First-Education/Address.cs
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/Address.cs
@@ -1,8 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Csharp_Code_First_Education
 {
+    [ComplexType]
     class Address
     {
+        public int AddressId { get; set; }
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
