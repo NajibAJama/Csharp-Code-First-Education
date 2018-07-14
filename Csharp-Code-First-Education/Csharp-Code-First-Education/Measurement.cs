a90a9399e57c5848dbe17d3281071146daab5779
NajibAJama <Najib.A.Jama@gmail.com>
2018-07-14 03:46:48 -0700

    created two new types, PersonalInfo and Measurement

---
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/Measurement.cs
@@ -0,0 +1,8 @@
+﻿namespace Csharp_Code_First_Education
+{
+    class Measurement
+    {
+        public decimal Reading { get; set; }
+        public string Units { get; set; }
+    }
+}
---
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/PersonalInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Csharp_Code_First_Education
+{
+    class PersonalInfo
+    {
+        public Measurement Weight { get; set; }
+        public Measurement Height { get; set; }
+        public string DietryRestrictions { get; set; }
+    }
+}
