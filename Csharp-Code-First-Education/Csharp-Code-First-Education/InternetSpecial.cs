82eb0ac68605c6414380ab9b07743bc0df5de5f3
NajibAJama <Najib.A.Jama@gmail.com>
2018-07-15 01:48:11 -0700

    added more properties to internet special class

--- Csharp-Code-First-Education/Csharp-Code-First-Education/InternetSpecial.cs
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/InternetSpecial.cs
@@ -1,8 +1,14 @@
-﻿namespace Csharp_Code_First_Education
+﻿using System;
+namespace Csharp_Code_First_Education
 {
     class InternetSpecial
     {
         public int InternetSpecialId { get; set; }
         public int Nights { get; set; }
+        public decimal CostUSD { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public int AccommodationId { get; set; }
+        public Lodging Accommodation { get; set; }
     }
 }
