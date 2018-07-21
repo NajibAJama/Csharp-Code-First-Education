2b1feba9b0e512bdb3abb25c2c608f73d130468b
NajibAJama <Najib.A.Jama@gmail.com>
2018-07-21 16:39:12 -0700

    Created Resort model class that inherits from Lodging model class with two properties added

--- Csharp-Code-First-Education/Csharp-Code-First-Education/Lodging.cs
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/Lodging.cs
@@ -8,7 +8,7 @@ namespace Csharp_Code_First_Education
         public int LodgingId { get; set; }
         public string Name { get; set; }
         public string Owner { get; set; }
-        public bool IsResort { get; set; }
+        //public bool IsResort { get; set; }
         public Destination Destination { get; set; }
         public decimal MilesFromNearestAirport { get; set; }
         public int DestinationId { get; set; }
---
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/Resort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Code_First_Education
+{
+    class Resort:Lodging
+    {
+        public string Entertainment { get; set; }
+        public string Activities { get; set; }
+    }
+}
