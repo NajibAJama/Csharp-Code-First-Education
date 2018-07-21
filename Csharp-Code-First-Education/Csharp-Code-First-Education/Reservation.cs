3cde231b609cc8bde40338e150a3cdc8e41f6205
NajibAJama <Najib.A.Jama@gmail.com>
2018-07-21 00:29:08 -0700

    Using Data Annotations to exclude a type from the model

--- Csharp-Code-First-Education/Csharp-Code-First-Education/Reservation.cs
+++ Csharp-Code-First-Education/Csharp-Code-First-Education/Reservation.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Csharp_Code_First_Education
 {
+    [NotMapped]
     class Reservation
     {
         public int ReservationId { get; set; }
